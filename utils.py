6a19ecfe867467f2b7b4a98b1eed928a160579e7
uroojmjoyo <124691973+uroojmjoyo@users.noreply.github.com>
2023-05-29 19:37:54 +0500

    utils.py

--- utils.py
+++ utils.py
@@ -4,8 +4,7 @@
 # In[5]:
 
 
-import pandas as pd 
-# Makes sure we see all columns
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 class DataLoader():
@@ -15,14 +14,21 @@ class DataLoader():
     def load_dataset(self, path="WTI Price.csv"):
         self.data = pd.read_csv(path)
 
-        # Standardization 
+        # Standardization
         # Usually we would standardize here and convert it back later
         # But for simplification we will not standardize / normalize the features
 
+    def preprocess_data(self):
+        # Drop 'DATE' column
+        self.data.drop('DATE', axis=1, inplace=True)
+        # Fill null values with column means
+        self.data.fillna(self.data.mean(), inplace=True)
+
     def get_data_split(self):
-        X = self.data.iloc[:,:-1]
-        y = self.data.iloc[:,-1]
-        return train_test_split(X, y, test_size=0.30)
+        self.preprocess_data()
+        X = self.data.drop('WTI_Spot', axis=1).copy()
+        y = self.data['WTI_Spot'].copy()
+        return train_test_split(X, y, test_size=0.3)
 
 
 # In[ ]:
